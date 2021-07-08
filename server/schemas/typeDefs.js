16070d514810df9d5e0480a76fc893e013b946d2
Emily Salinas <emilyrh1058@gmail.com>
2021-07-07 22:13:42 -0500

    .

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -30,10 +30,13 @@ type Query {
   thought(_id: ID!): Thought
 }
 type Mutation {
-  login(email: String!, password: String!): User
-  addUser(username: String!, email: String!, password: String!): User
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+}
+type Auth {
+  token: ID!
+  user: User
 }
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
