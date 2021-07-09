3a0fd8c8224d826223bad3f5a9a0686bd11bbaf1
Emily Salinas <emilyrh1058@gmail.com>
2021-07-08 22:54:34 -0500

    changed path, asking LA about error

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./server/schemas');
-const db = require('./server/config/connection');
-const { authMiddleware } = require('./server/utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+const { authMiddleware } = require('../client/src/utils/auth');
 
 
 const PORT = process.env.PORT || 3001;
