49eb613068b3ba6927ee2c7de70fe16d3a682cab
Emily Salinas <emilyrh1058@gmail.com>
2021-07-11 22:37:17 -0500

    .

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
-
+  const [addUser, { error }] = useMutation(ADD_USER);
+  
   // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
